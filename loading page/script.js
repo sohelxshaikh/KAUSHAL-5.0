142f7a78e3e464cc019567153cd93e7ab4efc1a3
Sohail Shaikh <sohailhacker7786@gmail.com>
2023-09-18 12:01:21 +0530

    animation added

--- loading page/script.js
+++ loading page/script.js
@@ -1,3 +1,25 @@
+var $circle = $('.circle'),
+    $follow = $('.circle-follow');
+
+function moveCircle(e) {
+    TweenLite.to($circle, 0.3, {
+      x: e.clientX,
+      y: e.clientY
+  });
+    TweenLite.to($follow, 0.7, {
+      x: e.clientX,
+      y: e.clientY
+  });  
+}
+
+$(window).on('mousemove', moveCircle);
+
+
+
+
+
+
+
 var tl = gsap.timeline()
 
 tl
@@ -78,3 +100,61 @@ tl
         
         ease :"Power4.easeOut"
     })
+
+
+    window.onload = function () {
+        const bars = document.querySelector(".bars");
+        const menu = document.querySelector(".menu");
+        bars.addEventListener("click", function (e) {
+          this.classList.toggle("active");
+          if (this.classList.contains("active")) {
+            gsap.to(".menu", {
+              duration: 0.1,
+              display: "flex",
+              ease: "expo.in"
+            });
+            gsap.to(".navBefore", {
+              duration: 0.5,
+              marginLeft: "0",
+              ease: "expo.in"
+            });
+            gsap.to(".nav", {
+              duration: 0.8,
+              marginLeft: "0",
+              delay: 0.3,
+              ease: "expo.in"
+            });
+            gsap.to(".navigation", {
+              duration: 1,
+              opacity: "1",
+              delay: 0.8,
+              ease: "expo.in"
+            });
+          } else {
+            gsap.to(".navigation", {
+              duration: 0.2,
+              opacity: "0",
+              ease: "expo.in"
+            });
+            gsap.to(".nav", {
+              duration: 1,
+              marginLeft: "100%",
+              delay: 0.3,
+              ease: "expo.in"
+            });
+            gsap.to(".navBefore", {
+              duration: 1,
+              marginLeft: "100%",
+              delay: 0.5,
+              ease: "expo.in"
+            });
+            gsap.to(".menu", {
+              duration: 1,
+              display: "none",
+              delay: 1,
+              ease: "expo.in"
+            });
+          }
+        });
+      };
+      
